bd5bc47af518ac604213e8b6bbe28f2fc546137e
Codingitsesy <s22210595@student.unklab.ac.id>
2024-07-03 08:50:28 +0800

    Final

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,100 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Html</li>
+        <li>CSS</li>
+        <li>DataBAse</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Currently majoring in computers at Unklab University</li>
+        <li>Graduated from North Sulawesi Academy (SLA) school</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Hobby",
+    id: "hobby",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Playing Game(Mobile Legends, Chess, Clash of clash)</li>
+        <li>Eating</li>
+        <li>BasketBall</li>
+        <li>Reading</li>
+        <li>Sleep for 24/24</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a student who is currently studying at Klabat University, my
+            status is single, my age is still considered young, the thing I like
+            most is basketball and I am taking the Front_end class in summer
+            2024/June with lecturer Stenly Adam
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("hobby")}
+              active={tab === "hobby"}
+            >
+              {" "}
+              Hobby{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
